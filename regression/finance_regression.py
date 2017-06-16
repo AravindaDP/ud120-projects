d162347c19b47474179d1e086a2b764252632d3f
Pramuditha Aravinda <aravindadp@gmail.com>
2017-06-16 21:04:15 +0530

    Regression Mini-Project: Solution
    Regression Mini-Project solution code.

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -39,14 +39,56 @@ test_color = "b"
 ### "r" to differentiate training points from test points.
 
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit (feature_train,target_train)
 
-
-
-
+print "Bonus estimation from salary"
+print "Slope:",reg.coef_, " Intercept:",reg.intercept_
+print "Training score:",reg.score(feature_train,target_train)
+print "Test score:",reg.score(feature_test,target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
+for feature, target in zip(feature_test, target_test):
+    plt.scatter( feature, target, color=test_color ) 
+for feature, target in zip(feature_train, target_train):
+    plt.scatter( feature, target, color=train_color ) 
+
+### labels for the legend
+plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
+plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+
+
+
+
+### draw the regression line, once it's coded
+try:
+    plt.plot( feature_test, reg.predict(feature_test) )
+except NameError:
+    pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print "Using Test Set:"
+print "Slope:",reg.coef_, " Intercept:",reg.intercept_
+plt.xlabel(features_list[1])
+plt.ylabel(features_list[0])
+plt.legend()
+plt.show()
+
+### long_term_incentive
+print ""
+print "Bonus estimation from long_term_incentive"
+features_list = ["bonus", "long_term_incentive"]
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+target, features = targetFeatureSplit( data )
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+reg = linear_model.LinearRegression()
+reg.fit (feature_train,target_train)
+print "Test score:",reg.score(feature_test,target_test)
+
 for feature, target in zip(feature_test, target_test):
     plt.scatter( feature, target, color=test_color ) 
 for feature, target in zip(feature_train, target_train):
