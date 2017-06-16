39a7feca1411659b3ca49a94dfe51f601651abfc
Pramuditha Aravinda <aravindadp@gmail.com>
2017-06-16 21:02:54 +0530

    Datasets Questions: Answers
    Datasets and questions solutions.

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,20 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-
+print "Data points:", len(enron_data)
+print "Features:", len(enron_data[enron_data.keys()[0]])
+poi_count = sum(1 for x in enron_data.values() if x["poi"])
+print "POIs:", poi_count
+print "James Prentice Total Stock:",  enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "Messages from Wesley to POI:",  enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print "Jeffrey Stock options:",  enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+print "Jeffrey Payments:",  enron_data["SKILLING JEFFREY K"]["total_payments"]
+print "Kenneth Payments:",  enron_data["LAY KENNETH L"]["total_payments"]
+print "Fastow Payments:",  enron_data["FASTOW ANDREW S"]["total_payments"]
+print "Salary Data Count:", sum(1 for x in enron_data.values() if not (x["salary"] == "NaN"))
+print "Email Address Count:", sum(1 for x in enron_data.values() if not (x["email_address"] == "NaN"))
+missing_total_payments = sum(1 for x in enron_data.values() if (x["total_payments"] == "NaN"))
+print "Missing total payments:", missing_total_payments, " [", missing_total_payments*100/len(enron_data), "]%" 
+poi_missing_payments = sum(1 for x in enron_data.values() if (x["total_payments"] == "NaN" and x["poi"]))
+print "Missing POI payments:", poi_missing_payments, " [", poi_missing_payments*100/poi_count, "]%"
+#print "Fastow data:", enron_data["FASTOW ANDREW S"]
